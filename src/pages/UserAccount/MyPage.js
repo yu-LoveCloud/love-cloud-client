bd17c51f57a4ff2ea1f60eb405f65d17eaad7086
Yun Jinwon <dnj1510@gmail.com>
2024-11-13 20:14:38 +0900

    초대코드 api 연결

--- src/pages/UserAccount/MyPage.js
+++ src/pages/UserAccount/MyPage.js
@@ -106,9 +106,9 @@ function MyPage() {
                     <MenuList>
                         <div onClick={isCouple}>파트너 관리 페이지</div>
                     </MenuList>
-                    <MenuList>
+                    {/* <MenuList>
                         <Link to='/refundaccount' style={{color: 'inherit' , textDecoration: 'none' }}>환불 계좌 등록하기</Link>
-                    </MenuList>
+                    </MenuList> */}
                     <MenuList>
                         <Link to='/' style={{color: 'inherit' , textDecoration: 'none'}}>배송지 관리하기</Link>
                     </MenuList>
